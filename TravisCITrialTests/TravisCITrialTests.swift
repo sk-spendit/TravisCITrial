b8007c4a92e38c2c75bbdaf1cea68a5c44b5059b
Stefan Kerlin <sk@spendit.de>
2016-02-04 14:48:01 +0100

    new failed test

--- TravisCITrialTests/TravisCITrialTests.swift
+++ TravisCITrialTests/TravisCITrialTests.swift
@@ -25,7 +25,7 @@ class TravisCITrialTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        let expected = "Test String"
+        let expected = "Test  String"
         let real = ViewController.getTestString()
         XCTAssertEqual(real, expected)
     }
